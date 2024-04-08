f24243aad2b7b69cf1e5bfaf21f2160de11a29d3
Allen Guo <guosonglin001@gmail.com>
2024-04-08 23:27:31 +0800

    wip

--- t.sh
+++ t.sh
@@ -1,39 +1,24 @@
-# TODO: add conda cache volume
-
-docker run -d -it \
-    --name ca_dev_0 \
-    -v ssh:/root/.ssh \
-    -v repos:/repos \
-    -w /repos \
-    base:latest
-
-micromamba install -y cmake ninja
-micromamba install -y clangdev llvmdev lld
-micromamba install -y spirv-headers spirv-tools lit
-micromamba install -y zlib
+# micromamba install -y cmake ninja
+# micromamba install -y clangdev llvmdev lld
+# micromamba install -y spirv-headers spirv-tools lit
+# micromamba install -y zlib
 
 ################################################################################
 
 cmake \
     --no-warn-unused-cli \
-    -DCMAKE_BUILD_TYPE:STRING=Debug \
+    -DCMAKE_BUILD_TYPE:STRING=Release \
     -DCMAKE_INSTALL_PREFIX:STRING=$PWD/build/install \
-    -DCA_LLVM_INSTALL_DIR:STRING=$CONDA_PREFIX \
+    -DCA_LLVM_INSTALL_DIR:STRING=/Users/allen/micromamba/envs/mlir \
     -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
     -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/clang \
     -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/clang++ \
     -DCA_RISCV_ENABLED:BOOL=TRUE \
     -DCA_ENABLE_API=cl \
-    -S $PWD \
-    -B $PWD/build \
-    -G Ninja
+    -S $PWD -B $PWD/build -G Ninja
 
-cmake --build $PWD/build --config Debug --target all --
-cmake --build $PWD/build --config Debug --target install --
-
-# TODO: use clang from conda
-# -DCMAKE_C_COMPILER:FILEPATH=$CONDA_PREFIX/bin/clang \
-# -DCMAKE_CXX_COMPILER:FILEPATH=$CONDA_PREFIX/bin/clang++ \
+cmake --build $PWD/build --target all
+cmake --build $PWD/build --target install
 
 ################################################################################
 
